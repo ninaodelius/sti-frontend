2b64cc1ab22d90a84047e414a99a319c6c6b9c75
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-02-17 11:28:37 +0100

    fixat logofotballsaker

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,30 @@
+age();
+
 function age(){
     var xhr = new XMLHttpRequest()
     xhr.open("GET", "https://localhost:3001/football")
     // xhr.open("GET", "/js/data.json")
     xhr.onload = function() {
-        var data = JSON.parse(this.response)
-        console.log("jag kommer hit")
-        createTable(data)
+        var data = JSON.parse(this.response);
+        createTable(data);
     }
-    xhr.send()
+    xhr.send();
 }
 
 function createTable(data){
     var appElement = document.getElementById("app")
     var aTable = document.createElement("table")
     appElement.appendChild(aTable)
-    aTable.appendChild(createRow(data[0].name, data[0].points))
-    aTable.appendChild(createRow(data[1].name, data[1].points))
-    aTable.appendChild(createRow(data[2].name, data[2].points))
-    aTable.appendChild(createRow(data[3].name, data[3].points))
+    aTable.appendChild(createRow(data[0].name, data[0].points, data[0].logo))
+    aTable.appendChild(createRow(data[1].name, data[1].points, data[1].logo))
+    aTable.appendChild(createRow(data[2].name, data[2].points, data[2].logo))
+    aTable.appendChild(createRow(data[3].name, data[3].points, data[3].logo))
 
 }
 
-function createRow(name, points){
+function createRow(name, points, url){
     var aRow = document.createElement("tr")
+    aRow.appendChild(createImageCell(url))
     aRow.appendChild(createCell(name))
     aRow.appendChild(createCell(points))
     return aRow
@@ -31,4 +33,13 @@ function createCell(content){
     var aCell = document.createElement("td")
     aCell.innerHTML = content
     return aCell;
+}
+
+function createImageCell(url){
+    var aCell = document.createElement("td")
+    var anImage = document.createElement("img")
+    anImage.src=url;
+    anImage.classList.add("tableLogo")
+    aCell.appendChild(anImage)
+    return aCell
 }
